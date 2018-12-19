321d356c613ad7b907940e5825f3e95e997313f7
olygood <olivier.huttmacher@gmail.com>
2018-12-19 16:46:26 +0100

    Update fonction-flèché.m

--- fonction-flèché.m
+++ fonction-flèché.m
@@ -23,11 +23,11 @@ const me = {
 };
 me.presentFriend("kevin);
 ```
-- **fonction flèché**  
+- **fonction flèché avec 1 argument n'a pas de parenthèse**  
 ```
 const me2 = {
   name:"oli"
-  presentFriend: friend => "tu connais" + friend + "?";
+  presentFriend: friend => "tu connais" + friend + "?"; //friend sans parenthèse car un argument 
   me2.presentFriend("sacha");
   ```
   **function qui ne reçois pas d'argument**
@@ -45,18 +45,33 @@ const me2 = {
   };
   me.presentThreeFriends("jean","bernard","julien");  
   ```
-  - **un fonction flèché qui exécute du code : **  
+  - **un fonction flèché qui as un arguments et qui éxécute du code : **  
   ```
    const me4 = {
     name:"oli",
     presentExecute: friend => {
-      
- ```
-    }
+        const presentation  = "tu connais" + friend + "?";
+        console.log (presentation);
+     return presentation
   };
-  me.presentExecute(laura);
+  me.presentExecute("laura");
   ```
- 
+ - **une autre particularité dans les fonctions flèchéé leurs façon de traiter "THIS"**.
+   clasique function = function(){
+   console.log(this);
+  }
+const arrowFuction = () => console.log (this);
+   
+   const me = {
+    name:"oli,
+    presentClassic : classicFunction,
+    presentArrow : arrowFunction
+   }
+   me.presentClassic();
+   me.presentArrow();
+   - la classic donne l'objet Me et la fonction flèché donne l'objet window  
+    classic : le this c'est l'objet me  
+     flèché  : le this  
     
   
   
